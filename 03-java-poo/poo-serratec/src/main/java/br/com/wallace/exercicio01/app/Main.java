19336864bfea0084fd345db9506f8b6ef73f80bc
arrobateh <wallaceteh1@gmail.com>
2026-04-08 16:06:40 -0300

    refactor: arruma bagunca

--- 03-java-poo/poo-serratec/src/main/java/br/com/wallace/exercicio01/app/Main.java
+++ 03-java-poo/poo-serratec/src/main/java/br/com/wallace/exercicio01/app/Main.java
@@ -8,6 +8,8 @@ public class Main { //Declara a classe principal do programa;
         Scanner leia = new Scanner(System.in); // Cria um objeto da classe Scanner chamado 'leia';
         int qtdSaque = 0; // Variavel que recebe a quantidade de saques realizado;
         int limiteSaque = 3; // Define o limite de saques diario;
+        double saldoConta = 2000.00;
+        double limiteSaqueValor = 1000.00;
 
         do {
             // Cria o menu;
@@ -28,14 +30,16 @@ public class Main { //Declara a classe principal do programa;
                     System.out.println("\nDepositando um Valor");
                     break;
                 case 3:
-                    if (qtdSaque < limiteSaque) { // Verifica se a quantidade de saques realizado e menor que o limite diario;
+                    if (qtdSaque < limiteSaque) {
                         qtdSaque++;
-                        System.out.println("\nRealizando um Saque...");
-                    break;
-                    } else {
-                        System.out.println("\nLimite de Saques Atingido! Volte Amanha."); // Informa ao usuario que o limite de saques foi atingido;
-                        break;
+                        System.out.println("Digite o valor para sacar: R$ ");
+                        double valorSaque = leia.nextDouble();
+
+                        if (valorSaque > limiteSaqueValor) {
+                            System.out.println("Saldo insuficiente!");
+                        }
                     }
+                    break;
                 case 4:
                     System.out.println("\nSaindo do menu");
                     return;
---
+++ 03-java-poo/poo-serratec/src/main/java/br/com/wallace/exercicio01/service/regraSaque.java
@@ -0,0 +1,7 @@
+package br.com.wallace.exercicio01.service;
+
+public class regraSaque {
+    public void sacar (double valor) {
+        System.out.print("T");
+    }
+}
