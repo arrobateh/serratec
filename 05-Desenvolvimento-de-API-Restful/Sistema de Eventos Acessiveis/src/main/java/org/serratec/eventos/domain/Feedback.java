25c86532206fc9494bcd047edd2e223923dc4b8c
arrobateh <wallaceteh1@gmail.com>
2026-05-19 18:15:42 -0300

    feat(feedback): cria a classe feedback e toda sua estrutura

---
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/domain/Feedback.java
@@ -0,0 +1,26 @@
+package org.serratec.eventos.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "feedback")
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id-feedback")
+    private Long id;
+
+    @NotBlank(message = "O comentário é obrigatório.")
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Size(max = 1, message = "Valor inválido. Digite sua nota entre 1 e 5")
+    @Column(name = "nota")
+    private Integer nota;
+
+}
