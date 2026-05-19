93e3d03f85e41cfb6c147e3deaf8a3a637514976
arrobateh <wallaceteh1@gmail.com>
2026-05-19 17:38:49 -0300

    feat(evento): cria relação com a classe local e adiciona algumas validações nos atributos

--- 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/domain/Evento.java
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/domain/Evento.java
@@ -15,13 +15,20 @@ public class Evento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_evento")
     private Long id;
 
     @Size(min = 1, max = 100, message = "O nome do evento deve conter entre 1 e 100 caracteres.")
     @NotBlank(message = "O nome do evento é obrigatório.")
+    @Column(name = "nomeEvento")
     private String nome;
 
     @NotNull(message = "A data do evento é obrigatória.")
+    @Column(name = "dataEvento")
     private LocalDate dataEvento;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_localEvento")
+    private Local localEvento;
 
 }
