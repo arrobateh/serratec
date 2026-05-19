85b5e7e0a445729eb57377825aac7f1be63e7e42
arrobateh <wallaceteh1@gmail.com>
2026-05-19 17:25:55 -0300

    feat(local): cria e estrutura a classe local

---
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/domain/Local.java
@@ -0,0 +1,23 @@
+package org.serratec.eventos.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "local")
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_local")
+    private Long id;
+
+
+    @NotBlank(message = "O nome do local é obrigatório.")
+    @Column(name = "nome")
+    private String nomeLocal;
+
+    private Integer capacidadeMaxima;
+}
