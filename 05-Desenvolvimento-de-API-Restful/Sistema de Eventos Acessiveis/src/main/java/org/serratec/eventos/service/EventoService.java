7861afb42cfbb4c1306aebd399fd9dffe87359c4
arrobateh <wallaceteh1@gmail.com>
2026-05-20 00:00:46 -0300

    fix: corrige estrutura da regra de negocio do sistema

--- 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/service/EventoService.java
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/service/EventoService.java
@@ -9,6 +9,9 @@ import org.serratec.eventos.repository.LocalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class EventoService {
     @Autowired
@@ -16,6 +19,18 @@ public class EventoService {
 
     @Autowired
     private LocalRepository localRepository;
+
+    public List<EventoResponseDTO> listarTodos() {
+        return eventoRepository.findAll().stream()
+                .map(EventoResponseDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public EventoResponseDTO buscarPorId(Long id) {
+        Evento evento = eventoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Evento não encontrado com id: " + id));
+        return new EventoResponseDTO(evento);
+    }
 
     public EventoResponseDTO salvar(EventoResquestDTO eventoResquestDTO) {
         Local local = localRepository.findById(eventoResquestDTO.getIdLocal())
@@ -29,5 +44,27 @@ public class EventoService {
         evento = eventoRepository.save(evento);
 
         return new EventoResponseDTO(evento);
+    }
+
+    public EventoResponseDTO atualizar(Long id, EventoResquestDTO eventoResquestDTO) {
+        Evento evento = eventoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Evento não encontrado com id: " + id));
+
+        Local localEvento = localRepository.findById(eventoResquestDTO.getIdLocal())
+                .orElseThrow(() -> new RuntimeException("Local não encontrado com o ID: " + eventoResquestDTO.getIdLocal()));
+        evento.setNome(eventoResquestDTO.getNome());
+        evento.setDataEvento(eventoResquestDTO.getDataEvento());
+        evento.setLocalEvento(localEvento);
+
+        evento = eventoRepository.save(evento);
+
+        return new EventoResponseDTO(evento);
+    }
+
+    public void deletar(Long id) {
+        if (!eventoRepository.existsById(id)) {
+            throw new RuntimeException("Não é possível deletar. Evento não encontrado com id: " + id);
+        }
+        eventoRepository.deleteById(id);
     }
 }
--- 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/service/LocalService.java
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/service/LocalService.java
@@ -1,5 +1,6 @@
 package org.serratec.eventos.service;
 
+import org.serratec.eventos.domain.Evento;
 import org.serratec.eventos.domain.Local;
 import org.serratec.eventos.dto.request.LocalResquestDTO;
 import org.serratec.eventos.dto.response.LocalResponseDTO;
@@ -15,6 +16,18 @@ public class LocalService {
     @Autowired
     private LocalRepository localRepository;
 
+    public List<LocalResponseDTO> listarTodos() {
+        return localRepository.findAll().stream()
+                .map(LocalResponseDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public LocalResponseDTO buscarPorId(Long id) {
+        Local localEvento = localRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Local não encontrado com id: " + id));
+        return new LocalResponseDTO(localEvento);
+    }
+
     public LocalResponseDTO salvar(LocalResquestDTO localRequestDTO) {
         Local local = new Local();
         local.setNomeLocal(localRequestDTO.getNomeLocal());
@@ -25,9 +38,22 @@ public class LocalService {
         return new LocalResponseDTO(local);
     }
 
-    public List<LocalResponseDTO> listarTodos() {
-        return localRepository.findAll().stream()
-                .map(LocalResponseDTO::new)
-                .collect(Collectors.toList());
+    public LocalResponseDTO atualizar(Long id, LocalResquestDTO localRequestDTO) {
+        Local local = localRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Local não encontrado com id: " + id));
+
+        local.setNomeLocal(localRequestDTO.getNomeLocal());
+        local.setCapacidadeMaxima(localRequestDTO.getCapacidadeMaxima());
+
+        local = localRepository.save(local);
+
+        return new LocalResponseDTO(local);
+    }
+
+    public void deletar(Long id) {
+        if (!localRepository.existsById(id)) {
+            throw new RuntimeException("Impossivel deletar. Local não encontrado com id: " + id);
+        }
+        localRepository.deleteById(id);
     }
 }
