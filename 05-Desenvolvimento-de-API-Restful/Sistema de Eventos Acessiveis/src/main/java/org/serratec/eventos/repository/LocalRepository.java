25df1cb9a457723d5b024132228f1d49a59f8c5e
arrobateh <wallaceteh1@gmail.com>
2026-05-19 21:13:51 -0300

    feat(LocalRepository): cria o repositorio para a classe local

---
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/repository/LocalRepository.java
@@ -0,0 +1,10 @@
+package org.serratec.eventos.repository;
+
+import org.serratec.eventos.domain.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocalRepository extends JpaRepository<Local, Long> {
+
+}
