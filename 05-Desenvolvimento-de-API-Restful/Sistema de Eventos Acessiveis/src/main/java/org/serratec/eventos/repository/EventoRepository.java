89260161e824e57d8ecd25dfe9e31c566f1ca41e
arrobateh <wallaceteh1@gmail.com>
2026-05-19 21:12:06 -0300

    feat(EventoRepository): cria o repositorio para a classe evento

---
+++ 05-Desenvolvimento-de-API-Restful/Sistema de Eventos Acessiveis/src/main/java/org/serratec/eventos/repository/EventoRepository.java
@@ -0,0 +1,10 @@
+package org.serratec.eventos.repository;
+
+import org.serratec.eventos.domain.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+
+}
